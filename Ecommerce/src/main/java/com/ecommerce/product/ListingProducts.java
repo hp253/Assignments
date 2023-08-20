e998912b77dba07d7aa1a810f8d72e8b361b4d0c
hp253 <r.hariprasanth253@gmail.com>
2023-08-20 19:26:10 +0530

    Product Details Portal
    Ecommerce commit

---
+++ Ecommerce/src/main/java/com/ecommerce/product/ListingProducts.java
@@ -0,0 +1,19 @@
+package com.ecommerce.product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListingProducts {
+	
+	List<Product> plist=new ArrayList<>();
+
+	public List<Product> getPlist() {
+		return plist;
+	}
+
+	public void setPlist(List<Product> plist) {
+		this.plist = plist;
+	}
+	
+	
+}
